e9226c180dd8a021b3692a900a7a628f631fbc0a
cuisongliu <cuisongliu@qq.com>
2017-06-27 19:49:21 +0800

    新增序列生成的功能

---
+++ module/src/main/java/com/xinyuewulian/mapper/SerialNoMapper.java
@@ -0,0 +1,7 @@
+package com.xinyuewulian.mapper;
+
+import com.cuisongliu.springboot.core.mapper.MyMapper;
+import com.xinyuewulian.entity.SerialNo;
+
+public interface SerialNoMapper extends MyMapper<SerialNo> {
+}
---
+++ module/src/main/java/com/xinyuewulian/mapper/SerialTypeMapper.java
@@ -0,0 +1,7 @@
+package com.xinyuewulian.mapper;
+
+import com.cuisongliu.springboot.core.mapper.MyMapper;
+import com.xinyuewulian.entity.SerialType;
+
+public interface SerialTypeMapper extends MyMapper<SerialType> {
+}
