90e83d703deafd5b8d741038d80f8cc89fb870d1
cuisongliu <cuisongliu@qq.com>
2017-06-28 22:10:48 +0800

    mapper新增查询方法

--- module/src/main/java/com/xinyuewulian/mapper/SerialTypeMapper.java
+++ module/src/main/java/com/xinyuewulian/mapper/SerialTypeMapper.java
@@ -1,7 +1,12 @@
 package com.xinyuewulian.mapper;
 
 import com.cuisongliu.springboot.core.mapper.MyMapper;
+import com.xinyuewulian.bo.SerialTypeBo;
 import com.xinyuewulian.entity.SerialType;
 
+import java.util.List;
+
 public interface SerialTypeMapper extends MyMapper<SerialType> {
+
+    public List<SerialTypeBo> querySerialTypeList();
 }
